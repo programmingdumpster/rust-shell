3ce8038d179cf720d8f5aa6ef7be5715ced2d4c3
pdumpster <Mateusz.S-ki@proton.me>
2024-04-08 22:36:36 +0200

    Improving commands, splitting them to other file

---
+++ src/commands.rs
@@ -0,0 +1,106 @@
+use std::{
+    env::set_current_dir,
+    fs::{ self, File },
+    io::{ self, BufRead, BufReader, Read, Write },
+    path::PathBuf,
+};
+
+use walkdir::WalkDir;
+
+pub fn echo(args: &[&str]) -> io::Result<()> {
+    // Echo function
+    let text = args.join(" ");
+
+    println!("{}", text);
+
+    Ok(())
+}
+
+pub fn cat(file1: &str, file2: &str, name: &str) -> io::Result<()> {
+    // Concatenate two files
+    let mut file1 = File::open(file1)?;
+    let mut contents1 = String::new();
+    file1.read_to_string(&mut contents1)?;
+
+    let mut file2 = File::open(file2)?;
+    let mut contents2 = String::new();
+    file2.read_to_string(&mut contents2)?;
+
+    let mut fileout = File::create(name)?;
+
+    let data = contents1 + &contents2;
+
+    fileout.write_all(data.as_bytes())?;
+
+    Ok(())
+}
+
+pub fn ls(path: PathBuf) -> io::Result<()> {
+    // List files in a directory
+    let mut column_w = 0;
+    let mut filenames = Vec::new();
+    let entries = fs::read_dir(path)?;
+
+    for entry in entries {
+        let entry = entry?;
+        let filename_str = entry.file_name().to_string_lossy().to_string();
+        column_w = column_w.max(filename_str.len());
+        filenames.push(filename_str);
+    }
+
+    let terminal_w = term_size::dimensions().map_or(80, |(w, _)| w);
+    let n_colums = terminal_w / (column_w + 2);
+
+    for (i, filename) in filenames.into_iter().enumerate() {
+        print!("{:width$}", filename, width = column_w);
+        if (i + 1) % n_colums == 0 {
+            println!();
+        } else {
+            print!("   ");
+        }
+    }
+
+    Ok(())
+}
+
+pub fn find(item: &str, dir: PathBuf) -> io::Result<()> {
+    // Find a file
+
+    let dir_entires = WalkDir::new(dir);
+
+    for entry in dir_entires.into_iter().filter_map(|e| e.ok()) {
+        if entry.file_name().to_string_lossy().to_lowercase() == item.to_lowercase() {
+            let path = entry.path().to_string_lossy().to_string();
+            println!("{:<6}", path);
+        }
+    }
+    Ok(())
+}
+
+pub fn grep(text: &str, file: &str) -> io::Result<()> {
+    // Search for text in a file
+    let _file1 = File::open(file)?;
+    let reader = BufReader::new(_file1);
+
+    for (line_number, line) in reader.lines().enumerate() {
+        let line = line?;
+
+        if line.contains(text) {
+            println!("{}:{}, {}", file, line_number, line);
+        }
+    }
+
+    Ok(())
+}
+
+pub fn cd(dir: &str) -> io::Result<()> {
+    // Change directory
+    let home = home::home_dir().unwrap().to_string_lossy().to_string();
+
+    match dir {
+        ".." => set_current_dir("..")?,
+        "" => set_current_dir(home)?,
+        _ => set_current_dir(dir)?,
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 #![windows_subsystem = "console"]
 
-#[warn(unused_imports)]
-use std::io::{ self, Read, Write };
+mod commands;
+use std::io::{ self, Write };
+use std::path::PathBuf;
 use std::process::Command;
-use std::fs::{ self, File };
-use std::env::{ self, set_current_dir };
-use walkdir::WalkDir;
-use std::io::{ BufRead, BufReader, Result };
-use home::home_dir;
+use std::env::{ self };
+use commands::*;
+use sysinfo::System;
 
 fn main() {
-    cmds(); // Display available commands
     loop {
         print!("\n {:?} {:?} #  ", whoami::username(), env::current_dir().unwrap()); // Display username and current directory
         io::stdout().flush().expect("flushing err"); // Flush output buffer
@@ -34,213 +32,117 @@ fn main() {
 
                     "echo" => {
                         // Echo the input
-                        echo(args);
-                    }
-
-                    "clear" => {
-                        // Clear the console
-                        let _ = Command::new("clear").status();
-                        let _ = Command::new("cls").status();
+                        match args.len() {
+                            1 => if let Err(error) = echo(args) {
+                                eprintln!("Directory reading error: {}", error);
+                            }
+                            _ => println!("Usage: echo [text]"),
+                        }
                     }
 
                     "cat" => {
-                        // Concatenate two files
-                        if args.len() == 3 {
-                            match cat(args[0], args[1], args[2]) {
-                                Ok(_) => {}
-                                Err(error) => eprintln!("Merging error: {}", error),
+                        // Merges files
+                        match args.len() {
+                            3 => if let Err(error) = cat(args[0], args[1], args[2]) {
+                                eprintln!("Merging error: {}", error);
                             }
-                        } else {
-                            println!("Usage: grep [text] [file.txt]");
+                            _ => println!("Usage: cat [file1] [file2] [output_file]"),
                         }
                     }
 
                     "cd" => {
                         // Change directory
-                        if args.len() == 1 {
-                            match cd(args[0]) {
-                                Ok(_) => {}
-                                Err(error) => eprintln!("{}", error),
+                        match args.len() {
+                            1 => if let Err(error) = cd(args[0]) {
+                                eprintln!("Dir changing error: {}", error);
                             }
-                        } else {
-                            cd("").expect("dir not exists");
+                            _ => println!("Usage: cd [directory] "),
                         }
                     }
 
                     "grep" => {
                         // Search for text in a file
-                        if args.len() == 2 {
-                            let _ = grep(args[0], args[1]);
-                        } else {
-                            println!("Usage: grep [text] [file.txt]");
-                        }
-                    }
-
-                    "cmds" => {
-                        // Display available commands
-                        cmds();
-                    }
-
-                    "run" => {
-                        // Run an executable
-                        if args.len() == 1 {
-                            run(args[0]);
-                        } else {
-                            println!("Usage: run [executable] ");
+                        match args.len() {
+                            2 => if let Err(error) = grep(args[0], args[1]) {
+                                eprintln!("Grep error: {}", error);
+                            }
+                            _ => println!("Usage: grep [text] [file.txt]"),
                         }
                     }
 
                     "find" => {
                         // Find a file
-                        if args.len() == 1 {
-                            find(args[0]);
-                        } else {
-                            println!("Usage: find [filename] ");
+                        match args.len() {
+                            1 => if let Err(error) = find(args[0], PathBuf::from(args[1])) {
+                                eprintln!("finding error: {}", error);
+                            }
+                            _ => println!("Usage: find [item]"),
                         }
                     }
 
                     "ls" => {
                         // List files in a directory
-                        if args.len() == 1 {
-                            match ls(args[0]) {
-                                Ok(()) => {}
-                                Err(error) => eprintln!("something fucked up: {}", error),
+
+                        match args.len() {
+                            1 => if let Err(error) = ls(PathBuf::from(args[0])) {
+                                eprintln!("File reading error: {}", error);
                             }
-                        } else {
-                            let dir = env::current_dir();
-                            match ls(&dir.unwrap().to_string_lossy().to_string()) {
-                                Ok(()) => {}
-                                Err(error) => eprintln!("something fucked up: {}", error),
+
+                            0 => if let Err(error) = ls(PathBuf::from(env::current_dir().unwrap())) {
+                                eprintln!("File reading error: {}", error);
+                            }
+
+                            _ => println!("Usage: grep [text] [file.txt]"),
+                        }
+                    }
+
+                    "cmds" => {
+                        // Display available commands
+                        println!(
+                            "
+                    
+                           CMDS: 
+                                quit / exit
+                                echo [input]
+                                clear - clears console
+                                cat [file.text] [file.txt] [output file name]
+                                grep [text] [file]
+                                find [file] [directory]
+                                ls [directory]
+                                cd [directory]
+                                cmds - this output
+                    
+                        "
+                        );
+                    }
+
+                    "clear" => {
+                        // Clear the console
+                        match std::env::consts::OS {
+                            "windows" => {
+                                // WIN
+                                let _ = Command::new("CLS").status();
+                            }
+                            "linux" | "macos" => {
+                                // UNIX
+                                let _ = Command::new("clear").status();
+                            }
+                            _ => {
+                                // Forbidden OS
+                                println!(
+                                    "ur system: {}",
+                                    System::long_os_version().unwrap().to_string()
+                                );
                             }
                         }
                     }
 
                     _ => {
-                        println!("wtf is this command: {}", command);
+                        println!("Unknown command: {}", command);
                     }
                 }
             }
             None => println!(),
         }
     }
-}
-
-fn echo(args: &[&str]) {
-    // Echo function
-    let text = args.join(" ");
-
-    println!("{}", text)
-}
-
-fn cat(file1: &str, file2: &str, name: &str) -> io::Result<()> {
-    // Concatenate two files
-    let mut file1 = File::open(file1)?;
-    let mut contents1 = String::new();
-    file1.read_to_string(&mut contents1)?;
-
-    let mut file2 = File::open(file2)?;
-    let mut contents2 = String::new();
-    file2.read_to_string(&mut contents2)?;
-
-    let mut fileout = File::create(name)?;
-
-    let data = contents1 + &contents2;
-
-    fileout.write_all(data.as_bytes())?;
-
-    Ok(())
-}
-
-fn ls(path: &str) -> Result<()> {
-    // List files in a directory
-    let mut column_w = 0;
-    let mut filenames = Vec::new();
-    let entries = fs::read_dir(path)?;
-
-    for entry in entries {
-        let entry = entry?;
-        let filename_str = entry.file_name().to_string_lossy().to_string();
-        column_w = column_w.max(filename_str.len());
-        filenames.push(filename_str);
-    }
-
-    let terminal_w = term_size::dimensions().map_or(80, |(w, _)| w);
-    let n_colums = terminal_w / (column_w + 2);
-
-    for (i, filename) in filenames.into_iter().enumerate() {
-        print!("{:width$}", filename, width = column_w);
-        if (i + 1) % n_colums == 0 {
-            println!();
-        } else {
-            print!("   ");
-        }
-    }
-
-    Ok(())
-}
-
-fn find(item: &str) {
-    // Find a file
-    let dir_path = "/";
-    let dir_entires = WalkDir::new(dir_path);
-
-    for entry in dir_entires.into_iter().filter_map(|e| e.ok()) {
-        if entry.file_name().to_string_lossy().to_lowercase() == item.to_lowercase() {
-            let path = entry.path().to_string_lossy().to_string();
-            println!("{:<6}", path);
-        }
-    }
-}
-
-fn grep(text: &str, file: &str) -> io::Result<()> {
-    // Search for text in a file
-    let _file1 = File::open(file)?;
-    let reader = BufReader::new(_file1);
-
-    for (line_number, line) in reader.lines().enumerate() {
-        let line = line?;
-
-        if line.contains(text) {
-            println!("{}:{}, {}", file, line_number, line);
-        }
-    }
-
-    Ok(())
-}
-
-fn cmds() {
-    // Display available commands
-    println!(
-        "
-
-       CMDS: 
-            quit / exit
-            echo [input]
-            clear - clears console
-            cat [file.text] [file.txt] [name]
-            grep [text] [file.txt]
-            find [file]
-            ls [path]
-            cd [dir]
-            cmds - this output
-
-    "
-    );
-}
-
-fn cd(dir: &str) -> Result<()> {
-    // Change directory
-    let home = home_dir().unwrap().to_string_lossy().to_string();
-
-    match dir {
-        ".." => set_current_dir("..")?,
-        "" => set_current_dir(home)?,
-        _ => set_current_dir(dir)?,
-    }
-    Ok(())
-}
-
-fn run(executable: &str) {
-    // Run an executable
-    let _ = Command::new("./".to_owned() + executable).status();
 }
